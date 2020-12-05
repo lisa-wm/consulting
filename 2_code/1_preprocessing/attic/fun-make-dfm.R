598fb5a557a3619f895f2d071376b7369a34621e
Lisa Wimmer <wml1293@outlook.de>
2020-12-05 10:58:52 +0100

    implemented partial pipeline for preprocessing

---
+++ 2_code/1_preprocessing/attic/fun-make-dfm.R
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------------------------
+# CREATION OF A DOCUMENT-FEATURE MATRIX
+# ------------------------------------------------------------------------------
+
+# Purpose: create a document-feature matrix for sentiment analysis
+
+# HELPER FUNCTIONS -------------------------------------------------------------
+
+
+
+# TOP-LEVEL FUNCTIONS ----------------------------------------------------------
+
+make_dfm_unigram <- function(tokens, 
+                     min_termfreq, 
+                     stemming = FALSE,
+                     tfidf = TRUE) {
+  
+  dfmat <- quanteda::dfm(tokens, verbose = FALSE)
+  
+  if (stemming) dfmat <- dfmat %>% quanteda::dfm_wordstem(language = "german") 
+  
+  dfmat <- dfmat %>%
+    quanteda::dfm_trim(min_termfreq = min_termfreq) 
+  
+  if (tfidf == TRUE) dfmat <- dfmat %>% quanteda::dfm_tfidf()
+  
+  dfmat
+  
+}
+
