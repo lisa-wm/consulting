71004213894987185efa2ae189a2d33415d6266e
Lisa Wimmer <ru59seq3@production-rstudio-2.novalocal>
2021-01-27 15:59:07 +0100

    put topic ranking aside for now just cant find a fucking solution

--- 2_code/2_2_b_run_topic_modeling_keywordbased.R
+++ 2_code/2_2_b_run_topic_modeling_keywordbased.R
@@ -184,16 +184,11 @@ keywords_list <- lapply(
     }
     
     list(
-      keyword = names(keywords_derivatives)[k],
+      keyword = names(keywords_derivatives)[i],
       derivatives = derivatives,
       byterms = unname(remaining_byterms[1:n_byterms]))
 
     })
-
-# keywords_list_dict <- list(
-#   keywords = do.call(rbind, keywords_list)[, 1],
-#   derivatives = do.call(rbind, keywords_list)[, 2],
-#   byterms = do.call(rbind, keywords_list)[, 3])
 
 names(keywords_list) <- names(keywords_clean_available)
 
@@ -227,185 +222,232 @@ tweets_matches_none <- tweets_matches[
   by = seq_len(nrow(tweets_matches))
   ][n_matches == 0]
 
-tweets_matches_any <- tweets_matches[!(doc_id %in% tweets_matches_none$doc_id)] 
+tweets_matches_any <- tweets_matches[!(doc_id %in% tweets_matches_none$doc_id)]
 
-test <- lapply(
-  
-  seq_along(keywords_clean_available),
-  
-  function(i) {
-    
-    topic_i <- names(keywords_clean_available)[i]
-    cols_i <- names(tweets_matches_any)[which(
-      startsWith(names(tweets_matches_any), topic_i) | 
-        names(tweets_matches_any) == "doc_id")]
-    dt_i <- tweets_matches_any[
-      , ..cols_i
-      ][, sum_matches := sum(.SD),
-        .SDcols = -c("doc_id"), 
-        by = doc_id
-        ][sum_matches > 0]
-    
-  }
-  
-)
+# FIXME find a goddam fucking way to rank this
 
-match_cols <- names(tweets_matches)[sapply(tweets_matches, is.numeric)]
-
-tweets_matches[, `:=` (
-  n_matches = sum(.SD), 
-  n_topics_matched = sum(.SD > 0)),
-  .SDcols = match_cols,
-  by = seq_len(nrow(tweets_matches))]
-
-tweets_matches_none <- tweets_matches[
-  n_matches == 0
-  ][, topic_label := NA
-    ][, .(doc_id, topic_label)]
-
-tweets_matches_unambiguous <- tweets_matches[
-  n_matches > 0 & n_lists_matched == 1
-  ][, topic_label := which.max(.SD),
-    .SDcols = match_cols,
-    by = doc_id
-    ][, .(doc_id, topic_label)]
-
-tweets_matches_ambiguous <- tweets_matches[
-  n_matches > 0 & n_lists_matched != 1
-  ][, foo := ifelse(n_matches %% n_lists_matches)]
-
-# [, topic_label := ifelse(
-#       n_topics_matched > 1 & n_matches %% n_topics_matched > 0, 
-#       which.max(.SD), 0), 
-#       .SDcols = match_cols, 
-#       by = doc_id]
-
-# RETRIEVE POSITIONS IN TOPIC KEYWORD LISTS THAT ARE MATCHED -------------------
-
-# Recall that by-terms are ordered by number of co-occurrences with the keyword,
-# so match positions indicate topical congruence 
-
-# TODO make this faster
-
-topic_matches <- data.table::data.table(
-  doc_id = quanteda::docnames(tweets_dfm_tm))
-
-topic_matches[
-  
-  # Create column for every topic
-  
-  , sapply(keywords, function(i) sprintf("topic_%d", i)) 
-  
-  := lapply(
-    
-    seq_along(keywords),
-    
-    function(i) {
-      
-      this_dfm <- matches_per_keyword[[i]]
-      
-      # For each document-topic combination, extract binary match list
-      
-      matches_d <- lapply(
-        doc_id,
-        function (j) {
-          this_row <- quanteda::dfm_subset(this_dfm, docnames(this_dfm) == j)
-          this_df <- quanteda::convert(this_row, to = "data.frame")
-          tibble::column_to_rownames(this_df, "doc_id")})
-      
-      # For each document-topic combination, retrieve positions of matches
-      # This omits cases where words are mentioned twice in one tweet, which 
-      # should be really rare and not worth the effort to incorporate
-      
-      matches_d_pos <- lapply(
-        seq_along(matches_d), 
-        function(j) which(matches_d[[j]] == 1))
-      
-      # In case of multiple matches, calculate score to balance position and 
-      # number of matches
-      # TODO find better heuristic
-      
-      lapply(
-        seq_along(matches_d_pos), function(j) {
-          ifelse(
-            length(matches_d_pos[[j]]) <= 1,
-            matches_d_pos[[j]],
-            min(matches_d_pos[[j]]) / length(matches_d_pos[[j]]))})
-      
-      })]
-
-svDialogs::msg_box("done matching topics")
-
-save_rdata_files(topic_matches, folder = "2_code/2_topic_extraction")
-
-# ASSIGN TOPIC LABELS ----------------------------------------------------------
-
-load_rdata_files(topic_matches, folder = "2_code/2_topic_extraction")
-
-# Convert topic columns from list to numeric
-
-topic_cols <- sprintf("topic_%d", keywords)
-topic_matches[, c(topic_cols) := lapply(.SD, as.numeric), .SDcols = topic_cols]
-
-# Assign topic labels
-
-topic_matches[
-  , topic_label := which.min(.SD),
-  .SDcols = topic_cols,
-  by = seq_len(nrow(topic_matches))
-  ][, topic_name := keywords[topic_label]]
-
-# MAP TOPIC LABLES TO ORIGINAL DOCUMENTS ---------------------------------------
-
-# Extract docvars as data.table for modification (more convenient than quanteda 
-# implementation in data.frame format)
-
-docvars_dt <- as.data.table(quanteda::docvars(tweets_dfm_tm))
-
-# Create ID equivalent to what quanteda assigns internally when creating dfm
-# TODO insert setkey statement
-
-docvars_dt[, doc_id := quanteda::docid(tweets_dfm_tm)]
-
-# Append topic labels
-
-docvars_dt <- topic_matches[docvars_dt, on = "doc_id"]
-
-# Insert modified docvars back into corpus
-
-tweets_corpus_topics_keywordbased <- tweets_corpus
-quanteda::docvars(tweets_corpus_topics_keywordbased) <- 
-  as.data.frame(docvars_dt)
-
-# Save
-
-save_rdata_files(tweets_corpus_topics_keywordbased, "2_code")
-
-# EXTRACT SOME DESCRIPTIVE STATISTICS ------------------------------------------
-
-party_colors <- c(
-  "deepskyblue",
-  "chartreuse4",
-  "black",
-  "deeppink3",
-  "darkgoldenrod1",
-  "red"
-)
-
-# Tweets that could be assigned a topic label
-
-docvars(tweets_corpus_topics_keywordbased) %>% 
-  group_by(party, topic_name) %>%
-  mutate(topic_name = ifelse(is.na(topic_name), "99_none", topic_name)) %>%
-  ggplot(aes(x = topic_name)) + 
-  geom_bar()
-
-# Tweets per topic and party
-
-docvars(tweets_corpus_topics_keywordbased) %>% 
-  group_by(party, topic_name) %>% 
-  mutate(topic_name = ifelse(is.na(topic_name), "99_none", topic_name)) %>%
-  filter(topic_name != "99_none") %>% 
-  ggplot(aes(x = topic_name, fill = party)) +
-  geom_bar(position = "dodge") +
-  scale_fill_manual(values = party_colors)
+# topic_ranks <- lapply(
+#   
+#   c("keyword", "derivatives", "byterms"),
+#   
+#   function(i) {
+#     
+#     cols_i <- names(tweets_matches_any)[which(
+#       endsWith(names(tweets_matches_any), i) | 
+#         names(tweets_matches_any) == "doc_id")]
+#     
+#     dt_i <- tweets_matches_any[, ..cols_i]
+#     data.table::setnames(dt_i, c("doc_id", names(keywords_clean_available)))
+#     
+#     foo <- t(apply(dt_i[, .(corona, klima)], 1, frank))
+#     
+#     dt_i[, c("corona", "klima") := apply(dt_i[, .(corona, klima)], 1, frank)]
+#     
+#     dt_i[, rank := frank(.SD), .SDcols = names(keywords_clean_available), 
+#          by = doc_id]
+#     
+#   }
+#   
+# )
+# 
+# 
+# test <- lapply(
+#   
+#   seq_along(keywords_clean_available),
+#   
+#   function(i) {
+#     
+#     topic_i <- names(keywords_clean_available)[i]
+#     cols_i <- names(tweets_matches_any)[which(
+#       startsWith(names(tweets_matches_any), topic_i) | 
+#         names(tweets_matches_any) == "doc_id")]
+#     dt_i <- tweets_matches_any[
+#       , ..cols_i
+#       ][, sum_matches := sum(.SD),
+#         .SDcols = -c("doc_id"), 
+#         by = doc_id]
+#     
+#     list(
+#       keyword = dt_i[, .(doc_id, get(sprintf("%s.keyword", topic_i)))],
+#       derivatives = dt_i[, .(doc_id, get(sprintf("%s.derivatives", topic_i)))],
+#       byterms = dt_i[, .(doc_id, get(sprintf("%s.byterms", topic_i)))],
+#       sum_matches = dt_i[, .(doc_id, sum_matches)]
+#     )
+#     
+#     })
+# 
+# names(test) <- names(keywords_clean_available)
+# foo <- data.table::setDT(do.call(cbind, test))
+# 
+# foo_keywords <- data.table::setDT(do.call(rbind, test))
+# 
+# foo <- data.table(
+#   id = rep(1, 6),
+#   topic = c(rep(1, 3), rep(2, 3)),
+#   type = rep(c("k", "d", "b"), 2),
+#   matches = c(1, 0, 1, 0, 2, 1)
+# )
+# 
+# foo <- tweets_matches_any[1:20]
+# 
+# t(apply(foo, 1, rank))
+# 
+# match_cols <- names(tweets_matches)[sapply(tweets_matches, is.numeric)]
+# 
+# tweets_matches[, `:=` (
+#   n_matches = sum(.SD), 
+#   n_topics_matched = sum(.SD > 0)),
+#   .SDcols = match_cols,
+#   by = seq_len(nrow(tweets_matches))]
+# 
+# tweets_matches_none <- tweets_matches[
+#   n_matches == 0
+#   ][, topic_label := NA
+#     ][, .(doc_id, topic_label)]
+# 
+# tweets_matches_unambiguous <- tweets_matches[
+#   n_matches > 0 & n_lists_matched == 1
+#   ][, topic_label := which.max(.SD),
+#     .SDcols = match_cols,
+#     by = doc_id
+#     ][, .(doc_id, topic_label)]
+# 
+# tweets_matches_ambiguous <- tweets_matches[
+#   n_matches > 0 & n_lists_matched != 1
+#   ][, foo := ifelse(n_matches %% n_lists_matches)]
+# 
+# # [, topic_label := ifelse(
+# #       n_topics_matched > 1 & n_matches %% n_topics_matched > 0, 
+# #       which.max(.SD), 0), 
+# #       .SDcols = match_cols, 
+# #       by = doc_id]
+# 
+# # RETRIEVE POSITIONS IN TOPIC KEYWORD LISTS THAT ARE MATCHED -------------------
+# 
+# # Recall that by-terms are ordered by number of co-occurrences with the keyword,
+# # so match positions indicate topical congruence 
+# 
+# # TODO make this faster
+# 
+# topic_matches <- data.table::data.table(
+#   doc_id = quanteda::docnames(tweets_dfm_tm))
+# 
+# topic_matches[
+#   
+#   # Create column for every topic
+#   
+#   , sapply(keywords, function(i) sprintf("topic_%d", i)) 
+#   
+#   := lapply(
+#     
+#     seq_along(keywords),
+#     
+#     function(i) {
+#       
+#       this_dfm <- matches_per_keyword[[i]]
+#       
+#       # For each document-topic combination, extract binary match list
+#       
+#       matches_d <- lapply(
+#         doc_id,
+#         function (j) {
+#           this_row <- quanteda::dfm_subset(this_dfm, docnames(this_dfm) == j)
+#           this_df <- quanteda::convert(this_row, to = "data.frame")
+#           tibble::column_to_rownames(this_df, "doc_id")})
+#       
+#       # For each document-topic combination, retrieve positions of matches
+#       # This omits cases where words are mentioned twice in one tweet, which 
+#       # should be really rare and not worth the effort to incorporate
+#       
+#       matches_d_pos <- lapply(
+#         seq_along(matches_d), 
+#         function(j) which(matches_d[[j]] == 1))
+#       
+#       # In case of multiple matches, calculate score to balance position and 
+#       # number of matches
+#       # TODO find better heuristic
+#       
+#       lapply(
+#         seq_along(matches_d_pos), function(j) {
+#           ifelse(
+#             length(matches_d_pos[[j]]) <= 1,
+#             matches_d_pos[[j]],
+#             min(matches_d_pos[[j]]) / length(matches_d_pos[[j]]))})
+#       
+#       })]
+# 
+# svDialogs::msg_box("done matching topics")
+# 
+# save_rdata_files(topic_matches, folder = "2_code/2_topic_extraction")
+# 
+# # ASSIGN TOPIC LABELS ----------------------------------------------------------
+# 
+# load_rdata_files(topic_matches, folder = "2_code/2_topic_extraction")
+# 
+# # Convert topic columns from list to numeric
+# 
+# topic_cols <- sprintf("topic_%d", keywords)
+# topic_matches[, c(topic_cols) := lapply(.SD, as.numeric), .SDcols = topic_cols]
+# 
+# # Assign topic labels
+# 
+# topic_matches[
+#   , topic_label := which.min(.SD),
+#   .SDcols = topic_cols,
+#   by = seq_len(nrow(topic_matches))
+#   ][, topic_name := keywords[topic_label]]
+# 
+# # MAP TOPIC LABLES TO ORIGINAL DOCUMENTS ---------------------------------------
+# 
+# # Extract docvars as data.table for modification (more convenient than quanteda 
+# # implementation in data.frame format)
+# 
+# docvars_dt <- as.data.table(quanteda::docvars(tweets_dfm_tm))
+# 
+# # Create ID equivalent to what quanteda assigns internally when creating dfm
+# # TODO insert setkey statement
+# 
+# docvars_dt[, doc_id := quanteda::docid(tweets_dfm_tm)]
+# 
+# # Append topic labels
+# 
+# docvars_dt <- topic_matches[docvars_dt, on = "doc_id"]
+# 
+# # Insert modified docvars back into corpus
+# 
+# tweets_corpus_topics_keywordbased <- tweets_corpus
+# quanteda::docvars(tweets_corpus_topics_keywordbased) <- 
+#   as.data.frame(docvars_dt)
+# 
+# # Save
+# 
+# save_rdata_files(tweets_corpus_topics_keywordbased, "2_code")
+# 
+# # EXTRACT SOME DESCRIPTIVE STATISTICS ------------------------------------------
+# 
+# party_colors <- c(
+#   "deepskyblue",
+#   "chartreuse4",
+#   "black",
+#   "deeppink3",
+#   "darkgoldenrod1",
+#   "red"
+# )
+# 
+# # Tweets that could be assigned a topic label
+# 
+# docvars(tweets_corpus_topics_keywordbased) %>% 
+#   group_by(party, topic_name) %>%
+#   mutate(topic_name = ifelse(is.na(topic_name), "99_none", topic_name)) %>%
+#   ggplot(aes(x = topic_name)) + 
+#   geom_bar()
+# 
+# # Tweets per topic and party
+# 
+# docvars(tweets_corpus_topics_keywordbased) %>% 
+#   group_by(party, topic_name) %>% 
+#   mutate(topic_name = ifelse(is.na(topic_name), "99_none", topic_name)) %>%
+#   filter(topic_name != "99_none") %>% 
+#   ggplot(aes(x = topic_name, fill = party)) +
+#   geom_bar(position = "dodge") +
+#   scale_fill_manual(values = party_colors)
