ef33b2ec4f9c9f5ac2808d88083a0f166037733b
Lisa Wimmer <ru59seq3@production-rstudio-3.novalocal>
2021-01-29 09:59:28 +0100

    added ideas for subj det

--- 2_code/4_run_subjectivity_detection.R
+++ 2_code/4_run_subjectivity_detection.R
@@ -5,4 +5,9 @@
 # IN: data with topic labels and various features
 # OUT: data bearing sentiment
 
-# EXTRACT TWITTER-SPECIFIC FEATURES --------------------------------------------
+# X --------------------------------------------
+
+# ideas. keep sentences that contain adjectives, polarity clues, emojis, maybe
+# personal pronouns like "ich"
+# look for subjectivity clues dict
+# discard tweets w/ numbers?
