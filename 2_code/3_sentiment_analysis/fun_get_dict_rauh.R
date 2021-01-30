3b90d072b32cee796e9cac126bcda133f0ccddfa
Lisa Wimmer <ru59seq3@production-rstudio-2.novalocal>
2021-01-30 14:11:08 +0100

    added rauh dict

---
+++ 2_code/3_sentiment_analysis/fun_get_dict_rauh.R
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------------------------
+# CREATION OF SENTIMENT DICTIONARY BY RAUH
+# ------------------------------------------------------------------------------
+
+# PURPOSE: dictionary proposed by Rauh
+
+get_dict_rauh <- function() {
+  
+  load(here("2_code/0_external_data", "Rauh_SentDictionaryGerman.Rdata"))
+  
+  dict <- data.table::setDT(sent.dictionary)
+  rm(sent.dictionary)
+  data.table::setnames(dict, c("term", "polarity"))
+  
+  dict[, polarity := as.factor(as.numeric(polarity))]
+  levels(dict$polarity) <- c("negative", "positive")
+  
+  dict[
+    , term := remove_umlauts(tolower(term))
+  ][, term := SnowballC::wordStem(term, language = "de")]
+  
+  dict
+  
+}
