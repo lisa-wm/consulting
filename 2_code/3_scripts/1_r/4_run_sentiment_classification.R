c99361de0a7307dff0bdaa96ace7c9d3fc503340
Lisa Wimmer <ru59seq3@production-rstudio-3.novalocal>
2021-02-07 15:48:54 +0100

    drafted ppl incl cart classif

--- 2_code/3_scripts/1_r/4_run_sentiment_classification.R
+++ 2_code/3_scripts/1_r/4_run_sentiment_classification.R
@@ -9,9 +9,6 @@ data <- tweets_subjective[
   label != "none"
   ][, created_at := NULL]
 
-# cols_to_keep <- names(data)[c(1:54, 57:58, 104, 124:128)]
-# data <- data[, ..cols_to_keep]
-
 data$label <- as.factor(data$label)
 
 task <- mlr3::TaskClassif$new(
@@ -34,44 +31,53 @@ po_tm$param_set$values <- list(
   text_field = "text",
   doc_grouping_var = c("username", "year", "month"),
   prevalence = prevalence_formula,
-  max.em.its = 1L,
+  max.em.its = 5L,
   stopwords = make_stopwords(),
   K = 3L)
 
-graph <- Graph$new()$add_pipeop(po_tm)
+# result_tm <- graph$train(task)[[1]]
+# result_tm$data()
 
-result_tm <- graph$train(task)[[1]]
-result_tm$data()
-
-po_cart <- PipeOpLearner$new(learner = lrn("classif.rpart"))
-
-po_sel <- PipeOpSelect$new()
-po_sel$param_set$values$selector <- 
-  selector_invert(selector_type(c("character")))
-
-graph <- graph %>>% po_sel %>>% po_cart
-
-plot(graph, html = F)
-
-graph_learner <- GraphLearner$new(graph)
-
-graph_learner$train(task)
-res <- graph_learner$predict(task)
-res$confusion
-
-result_tm <- graph$train(task)[[1]]
-result_tm$data()
+po_sel_ge <- PipeOpSelect$new(id = "select_embedding")
+po_sel_ge$param_set$values$selector <- selector_name(c("topic_label", "text"))
+po_sel_ge_inv <- PipeOpSelect$new(id = "select_rest")
+po_sel_ge_inv$param_set$values$selector <- 
+  selector_invert(selector_name(c("topic_label", "text")))
 
 po_ge <- PipeOpMakeGloveEmbeddings$new()
 po_ge$param_set$values$dimension <- 3L
 po_ge$param_set$values$stopwords <- make_stopwords()
-graph <- Graph$new()$add_pipeop(po_ge)
 
-po_sel2 <- PipeOpSelect$new()
-po_sel2$param_set$values$selector <- selector_name(c("topic_label", "text"))
+po_nop <- PipeOpNOP$new()
+
+po_sel_cl <- PipeOpSelect$new(id = "select_classif")
+po_sel_cl$param_set$values$selector <- 
+  selector_invert(selector_type(c("character")))
+
+po_cart <- PipeOpLearner$new(learner = lrn("classif.rpart"))
 
 graph <- Graph$new()$add_pipeop(po_tm)
-graph <- graph %>>% po_sel2 %>>% po_ge
 
-result_ge <- graph$train(task)[[1]]
-result_ge$data()
+graph <- graph %>>% 
+  gunion(list(
+    po_sel_ge %>>% 
+      po_ge,
+    po_sel_ge_inv %>>% 
+      po_nop)
+    ) %>>%
+  po("featureunion") 
+
+plot(graph, html = FALSE)
+
+# res_preproc <- graph$train(task)[[1]]
+# res_preproc$data()
+
+graph_full <- graph %>>%
+  po_sel_cl %>>%
+  po_cart
+
+graph_learner <- GraphLearner$new(graph_full)
+
+graph_learner$train(task)
+res <- graph_learner$predict(task)
+res$confusion
